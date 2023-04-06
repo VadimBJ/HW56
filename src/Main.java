859afa66163f159288b64afd969e94c5f87f0e85
Vadim Balanciuc <b.vadik2017@gmail.com>
2023-04-06 12:47:33 +0200

    add Main code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
-public class Main {
-  public static void main(String[] args)  {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    System.out.print("Введите количество элементов: ");
+    int n = Integer.parseInt(br.readLine());
+
+    List<Integer> numbers = new ArrayList<>(n);
+    for (int i = 0; i < n; ++i) {
+      numbers.add(Integer.parseInt(br.readLine()));
+    }
+    Collections.sort(numbers);
+    Collections.reverse(numbers);
+    System.out.println(numbers);
+
+    System.out.print("Введите число для поиска: ");
+    int target = Integer.parseInt(br.readLine());
+
+    System.out.println("Результат: " + FindNumber.findNumberInList(numbers, target));
   }
 }
